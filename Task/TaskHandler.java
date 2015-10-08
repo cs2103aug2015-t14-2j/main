3c7f9ba735c4f1bd6107d49a64eaa4cb30997097
Jean Castillo <remix224@gmail.com>
2015-10-08 17:12:43 +0800

    Revert "Merge branch 'staging' of https://github.com/cs2103aug2015-t14-2j/main into staging"
    This reverts commit e547c8100639c8f546fe735a688c220cc2c04371, reversing
    changes made to 3addeffd485404cfb50aaaa2a2f295a7bff43baa.
    
    Undo automatic dev push

--- Task/TaskHandler.java
+++ Task/TaskHandler.java
@@ -285,6 +285,7 @@ public class TaskHandler {
 			
 			if(desc != null){
 				Task task = null;
+				if (venue != null)
 				if (startTime != null && endTime != null){
 					task = new Task(currentTaskId+1, desc, _startDate, _endDate, venue);//Event
 				} else if (deadlineDate != null){
