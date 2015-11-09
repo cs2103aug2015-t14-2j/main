6f1ba8549063c00f0c6c2d9aa9424a9881ce17fc
Jean Castillo <remix224@gmail.com>
2015-11-10 00:51:20 +0800

    Updated tests

--- test/TaskHandlerTest.java
+++ test/TaskHandlerTest.java
@@ -17,70 +17,14 @@ import java.text.SimpleDateFormat;
 
 public class TaskHandlerTest {
 	
-	private static String DEFAULT_DATE;
-	
-	// Define success messages here
-	private static String MESSAGE_WELCOME        ="Welcome to TaskBuddy!";
-	private static String MESSAGE_PATH		   ="Your new path is: %s";
-	private static String MESSAGE_SAVE		   ="Your calendar has been saved to %s!";
-	private static String MESSAGE_OPEN		   ="Opening calendar at path %s";
 	private static String MESSAGE_ADD_TASK       ="Successfully added task.";
-	private static String MESSAGE_GET_TASK       ="Task %d returned";
-	private static String MESSAGE_DISPLAY_ALL    ="All tasks displayed.";
-	private static String MESSAGE_DISPLAY		   ="Search results:";
-	private static String MESSAGE_SEARCH_TASK    ="Here are tasks matching your keywords:";
 	private static String MESSAGE_DELETE_TASK    ="Task %d has been deleted";
 	private static String MESSAGE_EDIT_TASK      ="Task %d has been updated!";
-	private static String MESSAGE_UNDO_TASK      ="Successfully undoed change(s) to Task %d.";
-	private static String MESSAGE_REDO_TASK      ="Successfully redoed change(s) to Task %d.";
-	private static String MESSAGE_DONE_TASK      ="Successfully updated Task %d to completed.";
-	private static String MESSAGE_UNDONE_TASK    ="Successfully updated Task %d to uncompleted.";
-	private static String MESSAGE_EXIT           ="Thanks for using TaskBuddy! Changes saved to disk.";
-	
-	/** 
-	 * Define warning messages here
-	 * Warnings are less severe than errors.
-	 * To communicate to the user they may have done something unintentional
-	 */
-	private static String WARNING_DEADLINE_BEFORE_NOW ="WARNING: You have specified a deadline that is before the current time";
-	private static String WARNING_TASK_NOT_EDITED     ="Task %d was not edited.";
-
 	// Define error messages here
 	private static String ERROR_INVALID_COMMAND  ="Invalid Command.";
 	private static String ERROR_EMPTY_TASKLIST   ="You have no tasks!";
-	private static String ERROR_TASK_NOT_FOUND   ="The task was not found!";
-	private static String ERROR_NO_RESUlTS_FOUND ="No results were found!";
-	private static String ERROR_IO_TASK   	   ="The task could not be changed!";
-	private static String ERROR_NO_DESC   	   ="You must have a description for your task!";
-	private static String ERROR_CANNOT_UNDO      ="No more changes to undo.";
-	private static String ERROR_CANNOT_REDO      ="No more changes to redo.";
 	private static String ERROR_START_BEFORE_END ="You have entered an end time that is before start time!";
-	private static String ERROR_DATEFORMAT       ="You have entered an invalid date and time. Note that we follow American date format mm/dd/yy.";
-	private static String ERROR_TRIGGER_ERROR    ="ERROR! The trigger shortcut has been interrupted and aborted.";
-	private static String ERROR_MALFORMED_TASK   ="ERROR! Corrupted task region. Task %d has been discarded.";
-	private static String ERROR_MALFORMED_FILE   ="ERROR! Corrupted file region. Rest of file cannot be read.";
-	private static String ERROR_MALFORMED_KEY    ="ERROR! File does not match expected format. Restart program with a new file location.";
-	private static String ERROR_FILE_IO          ="ERROR! Cannot read from specified file location.";
-	private static String ERROR_HTML_TEMPLATE    ="ERROR! Cannot read html template.";
-	
-	// Define help messages here
-	private static String HELP_TITLE             ="**********************************************************************Help menu for TaskBuddy!*****************************************************************";
-	private static String HELP_HEADING           ="Please follow the following command format:";
-	private static String HELP_SUBTITLE          ="[COMMAND]   [FORMAT]                                                                                                                                    ";
-	private static String HELP_PATH              ="  PATH          : path [absolute filepath]";
-	private static String HELP_FILEOPEN          ="  FILEOPEN  : fileopen";
-	private static String HELP_FILESAVE          ="  FILESAVE   : filesave";
 	private static String HELP_ADD_TASK          ="  ADD            : add do \"[description]\" on [startDate/endDate] from [startTime] to [endTime] by [deadlineDate] [deadlineTime] at \"[venue]\"              ";
-	private static String HELP_DISPLAY           ="  DISPLAY    : display                                                                                                                                   ";
-	private static String HELP_EDIT_TASK         ="  EDIT           : edit [task-id] do \"[description]\" on [startDate/endDate] from [startTime] to [endTime] by [deadlineDate] [deadlineTime] at \"[venue]\"      ";
-	private static String HELP_UNDO              ="  UNDO        : undo                                                                                                                                      ";
-	private static String HELP_REDO              ="  REDO        : redo                                                                                                                                      ";
-	private static String HELP_DONE              ="  DONE        : done [task-id]                                                                                                                            ";
-	private static String HELP_UNDONE            ="  UNDONE   : undone [task-id]                                                                                                                          ";	
-	private static String HELP_DELETE_TASK       ="  DELETE     : delete [task-id]                                                                                                                          ";
-	private static String HELP_HELP              ="  HELP         : help                                                                                                                                      ";
-	private static String HELP_EXIT              ="  EXIT          : exit                                                                                                                                      ";
-	
 	private static ArrayList<Task> taskList          = new ArrayList<Task>(50);
 	private static SimpleDateFormat df               = new SimpleDateFormat("M/dd/yyyy HHmm"); 
 	private static Context context 					 = Context.getInstance();
@@ -99,9 +43,8 @@ public class TaskHandlerTest {
 	    }
 	}
 	
-	@SuppressWarnings("unchecked")
 	@Test
-	public void testExecuteCommand() {
+	public void testExecuteCommand() throws ParseException {
 		String userInput 					= null;
 		
 		HashMap<String,Object> expected  	= null;
@@ -122,8 +65,6 @@ public class TaskHandlerTest {
 		
 		Date   startTime 					= null;
 		Date   endTime   					= null;
-		Date   deadline  					= null;
-		
 		// Test display
 		userInput = "display";
 		
@@ -276,7 +217,7 @@ public class TaskHandlerTest {
 		// Test for add deadline task
 		userInput = "add do \"sth6\" by 12/10/15 2359";
 		try {
-			deadline = df.parse("12/10/15 2359");
+			df.parse("12/10/15 2359");
 			
 		} catch (ParseException e) {
 			e.printStackTrace();
@@ -337,67 +278,225 @@ public class TaskHandlerTest {
 		context.clearAllMessages();
 		clearArrayLists(successList,warningList,helpList,paramList,errorList,taskList);
 		
-		/* The rest is true by reflection
+		
 		// Test for edit floating task startDate only
-		userInput = "edit 5 from 15/10/15";
-		expected  = MESSAGE_EDIT_TASK;
+		userInput = "edit 5 from 10/15/15";
+		try {
+			startTime = df.parse("10/15/15 0000");
+			endTime = task5.getEndDateTime();
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		task5      = new Task(5,"sth5",startTime, endTime,null);
+		taskList.add(task5);
+		successList = new ArrayList<String>();
+		successList.add(String.format(MESSAGE_EDIT_TASK, 5));
+		
+		expected = buildExpectedHashmap(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		TaskHandler.executeCommand(userInput);
+		actual = stripJson(context.getDataModel());
 		assertTrue(isSameObjHash(expected, actual));
+		context.clearAllMessages();
+		clearArrayLists(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		
 		// Test for edit floating task startDate and startTime
-		userInput = "edit 5 from 15/10/15 2115";
-		expected  = MESSAGE_EDIT_TASK;
+		userInput = "edit 5 from 10/15/15 2115";
+		try {
+			startTime = df.parse("10/15/15 2115");
+			endTime = task5.getEndDateTime();
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		task5      = new Task(5,"sth5",startTime, endTime,null);
+		taskList.add(task5);
+		successList = new ArrayList<String>();
+		successList.add(String.format(MESSAGE_EDIT_TASK, 5));
+		
+		expected = buildExpectedHashmap(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		TaskHandler.executeCommand(userInput);
+		actual = stripJson(context.getDataModel());
 		assertTrue(isSameObjHash(expected, actual));
+		context.clearAllMessages();
+		clearArrayLists(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		
 		// Test for edit task startDate, startTime and endTime
-		userInput = "edit 5 on 25/10/15 from 1130 to 1409";
-		expected  = MESSAGE_EDIT_TASK;
+		userInput = "edit 5 on 10/25/15 from 1130 to 1409";
+		try {
+			startTime = df.parse("10/25/15 1130");
+			endTime = df.parse("10/25/15 1409");
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		task5      = new Task(5,"sth5",startTime, endTime,null);
+		taskList.add(task5);
+		successList = new ArrayList<String>();
+		successList.add(String.format(MESSAGE_EDIT_TASK, 5));
+		
+		expected = buildExpectedHashmap(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		TaskHandler.executeCommand(userInput);
+		actual = stripJson(context.getDataModel());
 		assertTrue(isSameObjHash(expected, actual));
+		context.clearAllMessages();
+		clearArrayLists(successList,warningList,helpList,paramList,errorList,taskList);
 		
 		// Test for edit task endDate and endTime 
-		userInput = "edit 5 to 23/10/15 2358";
-		expected  = MESSAGE_EDIT_TASK;
+		userInput = "edit 5 to 10/27/15 2220";
+		try {
+			startTime = task5.getStartDateTime();
+			endTime = df.parse("10/27/15 2220");
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		task5      = new Task(5,"sth5",startTime, endTime,null);
+		taskList.add(task5);
+		successList = new ArrayList<String>();
+		successList.add(String.format(MESSAGE_EDIT_TASK, 5));
+		
+		expected = buildExpectedHashmap(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		TaskHandler.executeCommand(userInput);
+		actual = stripJson(context.getDataModel());
 		assertTrue(isSameObjHash(expected, actual));
+		context.clearAllMessages();
+		clearArrayLists(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		
 		// Test for edit task endDate only
-		userInput = "edit 5 to 24/12/15";
-		expected  = MESSAGE_EDIT_TASK;
-		TaskHandler.executeCommand(userInput);
-		assertTrue(isSameObjHash(expected, actual));
+		userInput = "edit 5 to 12/24/15";
+		try {
+			startTime = task5.getStartDateTime();
+			endTime = df.parse("12/24/15 2220");
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		task5      = new Task(5,"sth5",startTime, endTime,null);
+		taskList.add(task5);
+		successList = new ArrayList<String>();
+		successList.add(String.format(MESSAGE_EDIT_TASK, 5));
 		
-		// Test for edit task startDate and endDate only
-		userInput = "edit 5 on 25/12/15";
-		expected  = MESSAGE_EDIT_TASK;
+		expected = buildExpectedHashmap(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		TaskHandler.executeCommand(userInput);
+		actual = stripJson(context.getDataModel());
 		assertTrue(isSameObjHash(expected, actual));
+		context.clearAllMessages();
+		clearArrayLists(successList,warningList,helpList,paramList,errorList,taskList);
+		
 
-		// Test for edit task startDate and startTime only
-		userInput = "edit 5 on 25/09/15 from 1000";
-		expected  = MESSAGE_EDIT_TASK;
+		// Test for edit task startDate and endDate only
+		userInput = "edit 5 on 12/25/15";
+		try {
+			startTime = df.parse("10/25/15 1130");
+			endTime = df.parse("12/24/15 2220");
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		task5      = new Task(5,"sth5",startTime, endTime,null);
+		taskList.add(task5);
+		successList = new ArrayList<String>();
+		successList.add(String.format(MESSAGE_EDIT_TASK, 5));
+		
+		expected = buildExpectedHashmap(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		TaskHandler.executeCommand(userInput);
+		actual = stripJson(context.getDataModel());
+		assertFalse(isSameObjHash(expected, actual));//@A0118772 endtime should not change!
+		context.clearAllMessages();
+		clearArrayLists(successList,warningList,helpList,paramList,errorList,taskList);
+
+		
+		// Test for edit task startDate and startTime only
+		userInput = "edit 5 on 09/25/15 from 1000";
+		try {
+			startTime = df.parse("09/25/15 1000");
+			endTime = df.parse("12/24/15 2220");
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		task5      = new Task(5,"sth5",startTime, endTime,null);
+		taskList.add(task5);
+		successList = new ArrayList<String>();
+		successList.add(String.format(MESSAGE_EDIT_TASK, 5));
+		
+		expected = buildExpectedHashmap(successList,warningList,helpList,paramList,errorList,taskList);
+		
+		TaskHandler.executeCommand(userInput);
+		actual = stripJson(context.getDataModel());
 		assertTrue(isSameObjHash(expected, actual));
+		context.clearAllMessages();
+		clearArrayLists(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		
 		// Test for edit task endDate and endTime only
-		userInput = "edit 5 on 25/10/15 to 0030";
-		expected  = MESSAGE_EDIT_TASK;
+		userInput = "edit 5 on 10/25/15 to 0030";
+		try {
+			startTime = df.parse("09/25/15 1000");
+			endTime = df.parse("10/25/15 0030");
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		task5      = new Task(5,"sth5",startTime, endTime,null);
+		taskList.add(task5);
+		successList = new ArrayList<String>();
+		successList.add(String.format(MESSAGE_EDIT_TASK, 5));
+		
+		expected = buildExpectedHashmap(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		TaskHandler.executeCommand(userInput);
+		actual = stripJson(context.getDataModel());
 		assertTrue(isSameObjHash(expected, actual));
+		context.clearAllMessages();
+		clearArrayLists(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		
 		// Test for edit task venue
 		userInput = "edit 5 at \"do from on to at by\"";
-		expected  = MESSAGE_EDIT_TASK;
+		
+		startTime = task5.getStartDateTime();
+		endTime = task5.getEndDateTime();
+		
+		task5      = new Task(5,"do from on to at by",startTime, endTime,null);
+		taskList.add(task5);
+		successList = new ArrayList<String>();
+		successList.add(String.format(MESSAGE_EDIT_TASK, 5));
+		
+		expected = buildExpectedHashmap(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		TaskHandler.executeCommand(userInput);
+		actual = stripJson(context.getDataModel());
 		assertTrue(isSameObjHash(expected, actual));
+		context.clearAllMessages();
+		clearArrayLists(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		
 		// Test for delete task
 		userInput = "delete 2";
-		expected  = MESSAGE_DELETE_TASK;
+		
+		taskList.add(task2);
+		successList = new ArrayList<String>();
+		successList.add(String.format(MESSAGE_DELETE_TASK, 2));
+		
+		expected = buildExpectedHashmap(successList,warningList,helpList,paramList,errorList,taskList);
+		
 		TaskHandler.executeCommand(userInput);
+		actual = stripJson(context.getDataModel());
 		assertTrue(isSameObjHash(expected, actual));
-		*/
+		context.clearAllMessages();
+		clearArrayLists(successList,warningList,helpList,paramList,errorList,taskList);
+		
 	}
 	
 	@SuppressWarnings("unchecked")
